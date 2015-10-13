c61ad11b856c6e857f3fc165e6451b03add5665a
EWarringer <erikwarringer@gmail.com>
2015-10-13 18:19:16 -0400

    hound-fix-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,21 +5,11 @@ class User < ActiveRecord::Base
   has_many :user_skills
   has_many :questions
   has_many :skills, through: :user_skills
-  has_many :conversations, :foreign_key => :sender_id
+  has_many :conversations, foreign_key: :sender_id
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
          :omniauthable
-
-  after_create :create_default_conversation
-
-  private
-
-  # for demo purposes
-
-  def create_default_conversation
-    Conversation.create(sender_id: 1, recipient_id: self.id) unless self.id == 1
-  end
 end
