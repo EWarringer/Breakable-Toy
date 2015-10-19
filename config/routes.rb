59a306d1499652f6e9abc6e72b012a1b8faf5fba
EWarringer <erikwarringer@gmail.com>
2015-10-19 02:22:21 -0400

    heroku errors 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
-
   root 'homes#index'
-
-  get "/" => "homes#index"
-
 
   resources :conversations do
     resources :messages
